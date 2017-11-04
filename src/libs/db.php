6462af9dde1be49a906529aacc7b86ca320f4672
Yu <yuwenbin.dj@gmail.com>
2017-11-04 13:06:47 +0800

    Delete db.php

--- src/libs/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Pascal
- * Date: 2017/10/24
- * Time: 21:31
- */
-
-$db = new PDO('mysql:host=127.0.0.1;port=13306;dbname=imooc','root','D0ngJuan*90106');
